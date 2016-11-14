ae8ffcf2d5b0f61db7df5a5461297881a6fc1fc7
thomascaillier <thomascaillier@gmail.com>
2016-11-14 15:24:55 +0100

    rub

--- auto_recup/src/auto_recup/Main.java
+++ auto_recup/src/auto_recup/Main.java
@@ -1,9 +1,12 @@
 package auto_recup;
 
+import java.io.*;
+import org.jdom.*;
+import org.jdom.output.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello!");
 	}
 
