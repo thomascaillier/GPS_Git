b126234e6f3aaf595efc377c83cb5d00c396375f
thomascaillier <thomascaillier@gmail.com>
2016-11-14 16:00:34 +0100

    14//11 work auto recup

--- auto_recup/src/auto_recup/Main.java
+++ auto_recup/src/auto_recup/Main.java
@@ -1,13 +1,39 @@
 package auto_recup;
 
 import java.io.*;
-import org.jdom.*;
-import org.jdom.output.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-	}
+	private static Scanner sc;
+	private static File fichiersGPS[];
+	private static int pos;
 
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Saisir le dossier à analyser:\n");
+		String src = sc.nextLine();
+		File dossier = new File(src);
+		pos=0;
+		listPath(dossier);
+	}
+	
+	static void listPath(File path) {
+	    File files[];
+	 
+	    files = path.listFiles();
+	    Arrays.sort(files);
+	    for (int i = 0, n = files.length; i < n; i++) {
+	      System.out.println(files[i].toString());
+	      if (files[i].isDirectory()) {
+	        listPath(files[i]);
+	      }
+	      else{
+	    	  if(files[i].getExtension()=="TCX")
+	    	  fichiersGPS[pos]=files[i];
+	    	  pos++;
+	      }
+	    }
+	}
 }
