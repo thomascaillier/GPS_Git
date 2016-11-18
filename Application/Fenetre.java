a74880b108bfe076bcfa8916cb12f59c6c14384a
MelvynRn <melvynr@outlook.fr>
2016-11-18 11:02:26 +0100

    Add files via upload

--- Application/Fenetre.java
+++ Application/Fenetre.java
@@ -1,32 +1,55 @@
-import java.awt.BorderLayout;
-
+import javax . swing .*;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.Color; 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import javax.swing.JButton;
 
-public class Fenetre extends JFrame {
+public class Fenetre extends JFrame implements ActionListener
+	
+{
+	private JButton home;
+  public Fenetre()
+  {            
+    this.setTitle("Fenetre");
+    this.setSize(800, 600);
+    this.setLocationRelativeTo(null); 
+	  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  
+ 
+    JPanel pan = new JPanel();
+    pan.setBackground(new Color(0,51,102));        
+    this.setContentPane(pan);               
+    this.setVisible(true);
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	/**
-	 * 
-	 */
-	private JPanel contentPane;
-
-	/**
-	 * Create the frame.
-	 */
-	public Fenetre() {
-		this.setTitle("Fenetre");
-    		this.setSize(800, 600);
-    		this.setLocationRelativeTo(null); 
-	  	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel pan = new JPanel();
-  		  pan.setBackground(new Color(0,51,102));        
-   		 this.setContentPane(pan);               
-    		this.setVisible(true);
+	  JPanel pane = new JPanel();
+	  setLayout(null); 
+	  repaint (); 
+	 
+	  
+		home = new JButton(""); 
+	  home.setIcon(new ImageIcon("icon.png"));
+	  home.addActionListener(this);
+	  home.setBounds(30,15,60,60); 
+	  home.setBackground(new Color(0,51,102));
+	  this.add(home);
+	 
+	  
+	  
+}
+	
+	
+	public void actionPerformed(ActionEvent evt) 
+	{
+		Object source = evt.getSource();
+		if(source==home)
+		{
+			 Fenetre fen = new Fenetre();
+		}
 	}
-
 }
