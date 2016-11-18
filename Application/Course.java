e2abe6f5930704f9ef1d8e2d640df00dea7130db
MelvynRn <melvynr@outlook.fr>
2016-11-18 11:03:46 +0100

    Add files via upload

---
+++ Application/Course.java
@@ -0,0 +1,22 @@
+import javax . swing .*;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.Color; 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import javax.swing.JFrame;
+ 
+public class Course {
+  public static void main(String aprs[]) {
+	  Fenetre fen = new Fenetre();
+	  
+ 
+	  
+    
+  }  
+
+
+}
