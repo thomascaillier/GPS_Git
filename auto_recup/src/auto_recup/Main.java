9aaaf7f04ea6cd98c7b77b0597afefdb78447a93
thomascaillier <thomascaillier@gmail.com>
2016-11-18 11:10:23 +0100

    Final auto_recup

--- auto_recup/src/auto_recup/Main.java
+++ auto_recup/src/auto_recup/Main.java
@@ -4,39 +4,58 @@ import java.io.*;
 import org.apache.commons.io.IOUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
 
-	private static Scanner sc;
 	private static ArrayList<File> fichiersGPS = new ArrayList<File>();
+	public JTextField tfsrc;
+	public JTextField tfdst;
+	public JFrame fen;
+	public JPanel form;
+	public JButton go;
 
-	public static void main(String[] args) {
-		sc = new Scanner(System.in);
-		System.out.println("Saisir le dossier à analyser:");
-		String src = sc.nextLine();
-		File dossier = new File(src);
-		listPath(dossier);
-		System.out.println("Fin du programme");
+	public static void main(String[] args){
+		new Main();
 	}
 	
-	static void listPath(File path) {
+	public Main() {
+		fen = new JFrame("Auto_Recup");
+		form = new JPanel();
+		JLabel lsrc = new JLabel("Entrer le dossier source:");
+		tfsrc = new JTextField(38);
+		JLabel ldst = new JLabel("Entrer le dossier destination:");
+		tfdst = new JTextField(38);
+		go = new JButton("AUTORECUP->");
+		go.addActionListener(new goAction());
+		form.add(lsrc);
+		form.add(tfsrc);
+		form.add(ldst);
+		form.add(tfdst);
+		form.add(go);
+		fen.setSize(450,300); 
+		fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fen.setResizable(false);
+		fen.setLocationRelativeTo(null);
+		fen.setContentPane(form);
+		fen.setVisible(true);		
+	}
+	
+	static void listPath(File path, String des) {
 	    File files[];
-	 
 	    files = path.listFiles();
 	    Arrays.sort(files);
 	    for (int i = 0, n = files.length; i < n; i++) {
 	      if (files[i].isDirectory()) {
-	        listPath(files[i]);
+	        listPath(files[i],des);
 	      }
 	      else{
-	    	  if(getExtension(files[i]).equals(".tcx")||getExtension(files[i]).equals(".fit")){
+	    	  if(formatAccept(getExtension(files[i]))){
 	    	  fichiersGPS.add(files[i]);
 	    	  }
 	      }
 	    }
-	    System.out.println("Saisir le dossier de destination:");
-	    String des = sc.nextLine();
 	    if(!des.endsWith("/") && !des.endsWith("\\"))des+="/";
 	    for(int i=0; i<fichiersGPS.size(); i++){
 	    	String sortie = fichiersGPS.get(i).toString().substring(fichiersGPS.get(i).toString().lastIndexOf("\\"));
@@ -83,5 +102,34 @@ public class Main {
 			return false;
 		}
 		return true;
+    }
+	
+	private static boolean formatAccept(String ext){
+		ArrayList<String> extensions = new ArrayList<String>();
+		extensions.add(".tcx");
+		extensions.add(".fit");
+		extensions.add(".gpx");
+		extensions.add(".hrm");
+		extensions.add(".xml");
+		extensions.add(".sdf");
+		extensions.add(".itn");
+		extensions.add(".nike+");
+		extensions.add(".pwx");
+		extensions.add(".csv");
+		for(int i=0; i<extensions.size();i++){
+			if(ext.toUpperCase().equals(extensions.get(i).toUpperCase()))
+				return true;
+		}
+		return false;
+	}
+	
+	public class goAction implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+        	String src = tfsrc.getText();
+    		File dossier = new File(src);
+    		listPath(dossier,tfdst.getText());
+    		go.setEnabled(false);
+    		System.out.println("Fin du programme");
+        }
     }
 }
